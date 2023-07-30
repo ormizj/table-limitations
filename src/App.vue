3d3f4fa1fb1bca205f9e49aeb2e7d93c7ed09fa9
spiderpig60 <spiderpig60@gmail.com>
2023-07-30 20:16:38 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <DynamicTable />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DynamicTable from './components/DynamicTable.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DynamicTable
   }
 }
 </script>
 
 <style>
+body {
+  background-color: rgb(25, 25, 25);
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 100px;
+  display: flex;
+  justify-content: center;
 }
 </style>
---
+++ src/components/DynamicTable.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="dynamic-table">
+    <table>
+      <tr>
+        <th>Header 1</th>
+        <th>Header 2</th>
+        <th>Header 3</th>
+      </tr>
+      <tr>
+        <td>Data 1</td>
+        <td>Data 2</td>
+        <td>Data 3</td>
+      </tr>
+      <tr>
+        <td class="long">Long Data</td>
+      </tr>
+      <tr>
+        <td>Data 1</td>
+        <td>Data 2</td>
+        <td>Data 3</td>
+      </tr>
+
+    </table>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+table {
+  padding: 15px;
+  border: 1px solid white;
+  background-color: gray;
+}
+
+
+tr {
+  /* following styles don't work */
+  padding: 999px;
+  margin: 999px;
+  border: 999px solid white;
+}
+
+td {
+  /* margin doesn't work */
+  margin: 5px;
+
+  padding: 10px;
+  border: 1px solid white;
+}
+
+th {
+  padding: 10px;
+}
+
+.long {
+  /* making a long data for dynamic table doesn't work (makes all colums longer [columns width will be equal to the widest data]) */
+  width: 100px;
+}
+</style>
